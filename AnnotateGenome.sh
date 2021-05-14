d2caa317229c34edb4e998c459323e2d5ec27f3e
Zoe Clarke <41270914+14zac2@users.noreply.github.com>
2021-05-14 09:58:58 -0400

    Update AnnotateGenome.sh
    Added stringtie annotation info

--- AnnotateGenome.sh
+++ AnnotateGenome.sh
@@ -66,3 +66,130 @@ liftoff \
  -o from_SpeTri_scf.gff -p 15 \
  -f liftoffFeatures.txt \
  -m /path/to/minimap2 -flank 0.5 -copies
+
+# Second, use transcriptional evidence to locate transcribed genes
+
+# The first step of this process is downloading publically available woodchuck RNA-seq data
+# These reads are paired-end and 200bp which improves the accuracy of gene calling compared to short read sequences that
+# seem to predict more fragmented gene structure
+
+# Using SRA-toolkit v2.10.8
+# Retrieve and the liver reads
+prefetch SRR10172922
+# Unpack - this provides the two paired fastq files within the reads folder SRR10172922
+fasterq-dump --split-files SRR10172922.sra
+# Retrieve and unpack kidney
+prefetch SRR10172923
+fasterq-dump --split-files SRR10172923.sra
+# Retrieve and unpack spleen
+prefetch SRR10172924
+fasterq-dump --split-files SRR10172924.sra
+# Retrieve and unpack lung
+prefetch SRR10172925
+fasterq-dump --split-files SRR10172925.sra
+# Retrieve and unpack heart
+prefetch SRR10172926
+fasterq-dump --split-files SRR10172926.sra
+# Retrieve and unpack pancreas
+prefetch SRR10172930
+fasterq-dump --split-files SRR10172930.sra
+# Retrieve and unpack thymus
+prefetch SRR10172931
+fasterq-dump --split-files SRR10172931.sra
+
+# Now align these reads to the woodchuck genome sequence using hisat2 v2.2.1
+# Index the woodchuck genome; -p is the number of threads
+# hisat2-build-s specifies the building of a small index library and creates .ht2 extensions
+# This is used over the creation of a large index because the genome is less than 4 billion base pairs in length
+hisat2-build-s -p 48 WCK01_AAH20201022_F8-SCF.fasta WCK01_AAH20201022_F8-SCF
+# Align the reads
+# hisat2-align-s specifies that a small reference index is used
+# -p is the number of threads
+# --dta reports alignments tailored for transcript assemblers (e.g. stringtie)
+# -x is the base name of the reference genome index that was specified in hisat2-build-s (precedes the .ht2 extensions)
+# -1 and -2 specify the first and second mates of paired-end reads
+# -S is the output SAM alignment file
+hisat2-align-s -p 48 --dta -x WCK01_AAH20201022_F8-SCF \
+ -1 SRR10172922/SRR10172922.sra_1.fastq \
+ -2 SRR10172922/SRR10172922.sra_2.fastq \
+ -S SRR10172922_scf.sam
+hisat2-align-s -p 48 --dta -x WCK01_AAH20201023_F8-SCF \
+ -1 SRR10172923/SRR10172923.sra_1.fastq \
+ -2 SRR10172923/SRR10172923.sra_2.fastq \
+ -S SRR10172923_scf.sam
+hisat2-align-s -p 48 --dta -x WCK01_AAH20201022_F8-SCF \
+ -1 SRR10172924/SRR10172924.sra_1.fastq \
+ -2 SRR10172924/SRR10172924.sra_2.fastq \
+ -S SRR10172924_scf.sam
+hisat2-align-s -p 48 --dta -x WCK01_AAH20201022_F8-SCF \
+ -1 SRR10172925/SRR10172925.sra_1.fastq \
+ -2 SRR10172925/SRR10172925.sra_2.fastq \
+ -S SRR10172925_scf.sam
+hisat2-align-s -p 48 --dta -x WCK01_AAH20201022_F8-SCF \
+ -1 SRR10172926/SRR10172926.sra_1.fastq \
+ -2 SRR10172926/SRR10172926.sra_2.fastq \
+ -S SRR10172926_scf.sam
+hisat2-align-s -p 48 --dta -x WCK01_AAH20201022_F8-SCF \
+ -1 SRR10172930/SRR10172930.sra_1.fastq \
+ -2 SRR10172930/SRR10172930.sra_2.fastq \
+ -S SRR10172930_scf.sam
+hisat2-align-s -p 48 --dta -x WCK01_AAH20201022_F8-SCF \
+ -1 SRR10172931/SRR10172931.sra_1.fastq \
+ -2 SRR10172931/SRR10172931.sra_2.fastq \
+ -S SRR10172931_scf.sam
+ 
+# These alignments now needed to be converted into sorted BAM files to be used as input for stringtie
+# Using samtools v1.12
+# -@ is number of threads
+# -S specifies SAM input
+# -h include header in SAM output; might be irrelevant in this scenario
+# -u indicates uncompressed BAM output
+# -o is name of output file
+samtools view -@ 48 -Shu -o SRR10172922_scf.bam SRR10172922_scf.sam
+samtools sort -@ 48 -o SRR10172922_scf.sorted.bam SRR10172922_scf.bam
+samtools view -@ 48 -Shu -o SRR10172923_scf.bam SRR10172923_scf.sam
+samtools sort -@ 48 -o SRR10172923_scf.sorted.bam SRR10172923_scf.bam
+samtools view -@ 48 -Shu -o SRR10172924_scf.bam SRR10172924_scf.sam
+samtools sort -@ 48 -o SRR10172924_scf.sorted.bam SRR10172924_scf.bam
+samtools view -@ 48 -Shu -o SRR10172925_scf.bam SRR10172925_scf.sam
+samtools sort -@ 48 -o SRR10172925_scf.sorted.bam SRR10172925_scf.bam
+samtools view -@ 48 -Shu -o SRR10172926_scf.bam SRR10172926_scf.sam
+samtools sort -@ 48 -o SRR10172926_scf.sorted.bam SRR10172926_scf.bam
+samtools view -@ 48 -Shu -o SRR10172930_scf.bam SRR10172930_scf.sam
+samtools sort -@ 48 -o SRR10172930_scf.sorted.bam SRR10172930_scf.bam
+samtools view -@ 48 -Shu -o SRR10172931_scf.bam SRR10172931_scf.sam
+samtools sort -@ 48 -o SRR10172931_scf.sorted.bam SRR10172931_scf.bam
+
+# Use the sorted bam files as input for stringtie
+# The output is a genome annotation file
+# Using stringtie v2.1.3
+# -o is the name of the output gtf file
+# -p is the number of threads
+# -l is the name prefix for output transcripts
+stringtie SRR10172922_scf.sorted.bam \
+ -o stringtie_SRR10172922_scf.gtf -p 48 -l STRG22
+stringtie SRR10172923_scf.sorted.bam \
+ -o stringtie_SRR10172923_scf.gtf -p 48 -l STRG23
+stringtie SRR10172924_scf.sorted.bam \
+ -o stringtie_SRR10172924_scf.gtf -p 48 -l STRG24
+stringtie SRR10172925_scf.sorted.bam \
+ -o stringtie_SRR10172925_scf.gtf -p 48 -l STRG25
+stringtie SRR10172926_scf.sorted.bam \
+ -o stringtie_SRR10172926_scf.gtf -p 48 -l STRG26
+stringtie SRR10172930_scf.sorted.bam \
+ -o stringtie_SRR10172930_scf.gtf -p 48 -l STRG30
+stringtie SRR10172931_scf.sorted.bam \
+ -o stringtie_SRR10172931_scf.gtf -p 48 -l STRG31
+ 
+# To prepare for filtering the stringtie annotation sets with Mikado, validate splice junctions with Portcullis
+# Combine separate sorted bam files for input to Portcullis
+# -@ is the number of threads
+samtools merge -@ 48 alioto_scf_merged.sorted.bam \
+ SRR10172922_scf.sorted.bam \
+ SRR10172923_scf.sorted.bam \
+ SRR10172924_scf.sorted.bam \
+ SRR10172925_scf.sorted.bam \
+ SRR10172926_scf.sorted.bam \
+ SRR10172930_scf.sorted.bam \
+ SRR10172931_scf.sorted.bam
+
