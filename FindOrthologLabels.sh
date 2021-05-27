cecc6bffb7d4a11ba20a06582f2db56fd2c84b60
Zoe Clarke <41270914+14zac2@users.noreply.github.com>
2021-05-27 13:45:55 -0400

    Update FindOrthologLabels.sh
    Added up to creating cdsHeader conversion files

--- FindOrthologLabels.sh
+++ FindOrthologLabels.sh
@@ -42,3 +42,78 @@ gunzip GCF_000236235.1_SpeTri2.0_protein.faa.gz
 # -f points towards the directory in which the peptide sequence fasta files were contained
 python ~/OrthoFinder_source/orthofinder.py -t 48 -a 12 \
  -s ~/orthofinder_sc2/preOrthoFinderTreeProtNames3.txt -f protein_seqs
+
+# Before the results can be processed by OrthoFinder2, additional files need to be created associated protein IDs
+# with their associated gene symbols. This is because OrthoFinder2 gives results in terms of peptide-peptide relationships
+# and single-cell analysis relies on gene symbols.
+# Databases like Uniprot and BioMart were not used because they do not contain complete gene symbol and protein ID information
+# for all of the organisms involved in this analysis.
+# Instead, conversion files created by manipulating the headers of translated CDS fasta files from Refseq that contained
+# the required gene + protein information
+# First, download the translated CDS fasta file for a certain species
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.39_GRCh38.p13/GCF_000001405.39_GRCh38.p13_translated_cds.faa.gz
+gunzip GCF_000001405.39_GRCh38.p13_translated_cds.faa.gz
+# Grab header of this file
+grep '>' GCF_000001405.39_GRCh38.p13_translated_cds.faa > GRCh38.p13_cdsHeader.tsv
+# Get rid of everything up to gene name
+sed -i -e 's/>\(.*\)gene=//' GRCh38.p13_cdsHeader.tsv
+# Get rid of everything between gene and protein id
+sed -i -e 's/]\(.*\)protein_id=/\t/' GRCh38.p13_cdsHeader.tsv
+# Get rid of everything after protein id
+sed -i -e 's/].*//' GRCh38.p13_cdsHeader.tsv
+# Make sure all have gene and protein
+awk -F'\t' 'NF==2' GRCh38.p13_cdsHeader.tsv > temp.tsv && mv temp.tsv GRCh38.p13_cdsHeader.tsv
+# This has created a file that has the gene id in the first column and the protein id in the second column
+
+# Repeat this for mouse
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/635/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_translated_cds.faa.gz
+gunzip GCF_000001635.27_GRCm39_translated_cds.faa.gz
+grep '>' GCF_000001635.27_GRCm39_translated_cds.faa > GRCm39_cdsHeader.tsv
+sed -i -e 's/>\(.*\)gene=//' GRCm39_cdsHeader.tsv
+sed -i -e 's/]\(.*\)protein_id=/\t/' GRCm39_cdsHeader.tsv
+sed -i -e 's/].*//' GRCm39_cdsHeader.tsv
+awk -F'\t' 'NF==2' GRCm39_cdsHeader.tsv > temp.tsv && mv temp.tsv GRCm39_cdsHeader.tsv
+
+# Repeat this for yellow-bellied marmot
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/676/075/GCF_003676075.2_GSC_YBM_2.0/GCF_003676075.2_GSC_YBM_2.0_translated_cds.faa.gz
+gunzip GCF_003676075.2_GSC_YBM_2.0_translated_cds.faa.gz
+grep '>' GCF_003676075.2_GSC_YBM_2.0_translated_cds.faa > GSC_YBM_2.0_cdsHeader.tsv
+sed -i -e 's/>\(.*\)gene=//' GSC_YBM_2.0_cdsHeader.tsv
+sed -i -e 's/]\(.*\)protein_id=/\t/' GSC_YBM_2.0_cdsHeader.tsv
+sed -i -e 's/].*//' GSC_YBM_2.0_cdsHeader.tsv
+awk -F'\t' 'NF==2' GSC_YBM_2.0_cdsHeader.tsv > temp.tsv && mv temp.tsv GSC_YBM_2.0_cdsHeader.tsv
+
+# Repeat this for Alpine marmot
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/001/458/135/GCF_001458135.1_marMar2.1/GCF_001458135.1_marMar2.1_translated_cds.faa.gz
+gunzip GCF_001458135.1_marMar2.1_translated_cds.faa.gz
+grep '>' GCF_001458135.1_marMar2.1_translated_cds.faa > marMar2.1_cdsHeader.tsv
+sed -i -e 's/>\(.*\)gene=//' marMar2.1_cdsHeader.tsv
+sed -i -e 's/]\(.*\)protein_id=/\t/' marMar2.1_cdsHeader.tsv
+sed -i -e 's/].*//' marMar2.1_cdsHeader.tsv
+awk -F'\t' 'NF==2' marMar2.1_cdsHeader.tsv > temp.tsv && mv temp.tsv marMar2.1_cdsHeader.tsv
+
+# Repeat this for 13-lined ground squirrel
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/236/235/GCF_000236235.1_SpeTri2.0/GCF_000236235.1_SpeTri2.0_translated_cds.faa.gz
+gunzip GCF_000236235.1_SpeTri2.0_translated_cds.faa.gz
+grep '>' GCF_000236235.1_SpeTri2.0_translated_cds.faa > SpeTri2.0_cdsHeader.tsv
+sed -i -e 's/>\(.*\)gene=//' SpeTri2.0_cdsHeader.tsv
+sed -i -e 's/]\(.*\)protein_id=/\t/' SpeTri2.0_cdsHeader.tsv
+sed -i -e 's/].*//' SpeTri2.0_cdsHeader.tsv
+awk -F'\t' 'NF==2' SpeTri2.0_cdsHeader.tsv > temp.tsv && mv temp.tsv SpeTri2.0_cdsHeader.tsv
+# All of these files will now be read into an R script to convert the OrthoFinder peptide relationships to gene relationships
+
+# Another file that needs to be made is a "Homologene" style file for human-woodchuck
+# This will be used to convert human pathway analysis files (GMT files) to woodchuck pathway analysis files
+# This file will also be made in R, but a precursor file will be made in bash
+# The precursor is similar to the gene-protein cdsHeader files, but also includes the gene ID (a unique number representing the gene)
+grep '>' ../../../refseq_blast/human_blast/GCF_000001405.39_GRCh38.p13_translated_cds.faa > GRCh38.p13_cdsHeader_ID.tsv
+# Get rid of everything up to gene name
+sed -i -e 's/>\(.*\)gene=//' GRCh38.p13_cdsHeader_ID.tsv
+# Get rid of everything between gene name and gene id
+sed -i -e 's/]\(.*\)GeneID:/\t/' GRCh38.p13_cdsHeader_ID.tsv
+# Get rid of everything between gene id and protein id
+sed -i -e 's/]\(.*\)protein_id=/\t/' GRCh38.p13_cdsHeader_ID.tsv
+# Get rid of everything after protein id
+sed -i -e 's/].*//' GRCh38.p13_cdsHeader_ID.tsv
+# Make sure all have gene name, gene id, and protein by ensuring there are 3 columns
+awk -F'\t' 'NF==3' GRCh38.p13_cdsHeader_ID.tsv > temp.tsv && mv temp.tsv GRCh38.p13_cdsHeader_ID.tsv
